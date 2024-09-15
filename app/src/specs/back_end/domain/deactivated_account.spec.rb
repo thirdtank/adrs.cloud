cc08b1e392cbd8ae9256c5176e9e236136c01101
Dave Copeland <davec@naildrivin5.com>
2024-09-15 15:08:10 -0400

    more tests

---
+++ app/src/specs/back_end/domain/deactivated_account.spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+RSpec.describe DeactivateAccount do
+  describe "::new" do
+    context "account is deactivated" do
+      it "creates without issue" do
+        account = create(:account, :deactivated)
+
+        deactivated_account = described_class.new(account:)
+
+        expect(deactivated_account.active?).to eq(false)
+        expect(deactivated_account.external_id).to eq(account.external_id)
+      end
+    end
+    context "account is active" do
+      it "raises ArgumentError" do
+        account = create(:account, :active)
+
+        expect {
+          described_class.new(account:)
+        }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
