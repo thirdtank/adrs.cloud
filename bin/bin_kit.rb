3727611582b5932e14469c4cfbb7ca5adab77183
Dave Copeland <davec@naildrivin5.com>
2024-10-09 18:07:43 -0400

    fixup dockerfile

--- bin/bin_kit.rb
+++ bin/bin_kit.rb
@@ -1,12 +1,18 @@
 require "pathname"
 require "fileutils"
+require "open3"
 
 # We don't want the setup method to have to do all this error
 # checking, and we also want to explicitly log what we are
 # executing. Thus, we use this method instead of Kernel#system
 def system!(*args)
   log "Executing #{args}"
-  if system(*args)
+  out,err,status = Open3.capture3(*args)
+  log "STDOUT:"
+  $stdout.puts out
+  log "STDERR:"
+  $stderr.puts err
+  if status.success?
     log "#{args} succeeded"
   else
     log "#{args} failed"
