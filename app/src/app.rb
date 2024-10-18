2108fa8c797882184f502b68da6e6aa7480da7e5
Dave Copeland <davec@naildrivin5.com>
2024-10-18 16:34:16 -0400

    try to get Sidekiq working in prod

--- app/src/app.rb
+++ app/src/app.rb
@@ -11,6 +11,23 @@ class App < Brut::App
     Brut.container.override("session_class") do
       AppSession
     end
+    if Brut.container.project_env.production?
+      Sidekiq.configure_server do |config|
+        config.redis = {
+          url: ENV(ENV.fetch("REDIS_PROVIDER")),
+          # Per https://devcenter.heroku.com/articles/connecting-heroku-redis#connecting-in-ruby
+          ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+        }
+      end
+
+      Sidekiq.configure_client do |config|
+        config.redis = {
+          url: ENV(ENV.fetch("REDIS_PROVIDER")),
+          # Per https://devcenter.heroku.com/articles/connecting-heroku-redis#connecting-in-ruby
+          ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+        }
+      end
+    end
 
   end
 end
