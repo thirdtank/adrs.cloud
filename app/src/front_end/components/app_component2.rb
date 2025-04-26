0c9db528159f37058cf558a7d7ed45f2287197a3
Dave Copeland <davec@naildrivin5.com>
2025-04-25 22:41:11 -0400

    time

--- app/src/front_end/components/app_component2.rb
+++ app/src/front_end/components/app_component2.rb
@@ -36,10 +36,7 @@ class AppComponent2 < Phlex::HTML
 
   def time_tag(timestamp:nil,**component_options, &contents)
     args = component_options.merge(timestamp:)
-    clock= Thread.current.thread_variable_get(:request_context)[:clock]
-    raw(
-      safe(Brut::FrontEnd::Components::Time.new(**args,&contents).render(clock:).to_s)
-    )
+    render Brut::FrontEnd::Components::Time.new(**args,&contents)
   end
 
   def form_tag(**args, &block)
