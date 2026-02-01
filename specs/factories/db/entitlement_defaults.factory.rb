b0210277d81fca3b95273f5f0a80674c74ef7904
Dave Copeland <davec@naildrivin5.com>
2026-02-01 11:39:47 -0500

    avoid unique clashes

--- specs/factories/db/entitlement_defaults.factory.rb
+++ specs/factories/db/entitlement_defaults.factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :entitlement_default, class: "DB::EntitlementDefault" do
-    sequence(:internal_name) { |n| "#{Faker::Subscription.plan} - #{n}" }
+    internal_name { "#{Faker::Subscription.plan} - #{SecureRandom.uuid}" }
     max_non_rejected_adrs { rand(10) + 3 }
     max_projects { rand(10) + 3 }
   end
