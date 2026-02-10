f3870fdedf02df7717cab16b61be074954658275
Dave Copeland <davec@naildrivin5.com>
2026-02-10 10:45:16 -0500

    need platform

--- deploy/docker_config.rb
+++ deploy/docker_config.rb
@@ -1,23 +1,6 @@
 class DockerConfig
 
-  # Return a Hash oif additional images to run, beyond "web" and "release".
-  #
-  # The format of this Hash is:
-  #
-  # {
-  #   "«image name» => {
-  #     cmd: "«command line for Dockerfile RUN directive»",
-  #   }
-  # }
-  #
-  # For example, if you have the Sidekiq segment installed, `bin/run-sidekiq`
-  # runs Sidekiq, so you would return this hash:
-  #
-  # {
-  #   "sidekiq" => {
-  #     cmd: "bin/run-sidekiq",
-  #   }
-  # }
+  def platform = nil
   def additional_images
     {
       "sidekiq" => {
